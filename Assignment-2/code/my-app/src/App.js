d64795e1e6a1e68bc8c93fd20a9285e2ce0d59e0
Indra10x <kunduindranil0@gmail.com>
2022-02-08 13:01:23 +0530

    assignment 2 done

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -4,13 +4,35 @@ import './App.css';
 
 
 function App(props) {
- const {employee}=props;
-    
-return(
-  <div className="container">
-  {/* Start your React code here */}
-</div>
-)
+  const { employee } = props;
+
+  return (
+    <div className="container">
+      <div class="pro-img">
+        <img className="img-top" src=(employee.profileimg) width="100" height="100" alt="profile image" />
+      </div>
+      <div className="title">
+        <h1>(employee.name)</h1>
+      </div>
+      <div className="pro-info">
+        <label>location</label>
+        <strong>(employee.location)</strong>
+
+      </div>
+      <div className="pro-info">
+        <label>Blood group</label>
+        <strong>(employee.bloodgroup)</strong>
+
+
+      </div>
+      <div className="pro-info">
+        <label>Age</label>
+        <strong>(employee.age)</strong>
+
+
+      </div>
+    </div>
+  )
 }
 
 
